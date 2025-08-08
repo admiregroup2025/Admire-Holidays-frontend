66b8931d57963181bf1fe8745cca0867e0dd78bf
Harshit Kumar Tyagi <tyagiharshit720@gmail.com>
2025-08-08 16:01:24 +0530

    updated for testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env, // Explicitly pass environment variables
+  server: {
+    host: true, // Allow external access
   },
+  preview: {
+    host: true,
+  }
 });
